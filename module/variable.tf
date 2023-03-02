dee3120772b090b6b5d5e89df4263e210dba631d
Atisha Shaurya <94087803+Atisha-Shaurya@users.noreply.github.com>
2023-03-02 17:38:43 +0530

    Add files via upload

---
+++ module/variable.tf
@@ -0,0 +1,22 @@
+variable "resourcename" {
+  type = string
+  description = "this is resourcegroup"
+}
+variable "location" {
+  type = string
+}
+
+variable "storagename" {
+  type = string
+}
+variable "tags" {
+  type = map(any)
+}
+variable "containername" {
+  type = string
+}
+variable "blobstorage" {
+  type = string
+}
+
+
